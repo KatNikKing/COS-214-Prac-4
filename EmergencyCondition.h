c6380c9b997c05a610f828218788b16e81954b0c
Markus Zeelie <mmzzeelie@gmail.com>
2026-09-05 07:35:22 +0200

    Added EmergencyCondition.h

---
+++ EmergencyCondition.h
@@ -0,0 +1,24 @@
+#ifndef EMERGENCYCONDITION_H
+#define EMERGENCYCONDITION_H
+
+#include "Emergency.h"
+
+
+class EmergencyCondition : public EmergencyCondition
+{
+    //members
+    protected:
+        Emergency* emergency;
+
+    //methods
+    public:
+        EmergencyCondition(*Emergency);
+        virtual ~EmergencyCondition();
+
+        virtual std::vector<Capability> getRequiredCapabilities() = 0;
+        std::string getLocation();
+        std::string getDescription();
+
+};
+
+#endif
