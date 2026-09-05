6d80625b95967f85221f659034d03317ea964b6a
Markus Zeelie <mmzzeelie@gmail.com>
2026-09-05 07:28:48 +0200

    Added EmergencyIncident.h

---
+++ EmergencyIncident.h
@@ -0,0 +1,28 @@
+#ifndef EMERGENCYINCIDENT_H
+#define EMERGENCYINCIDENT_H
+
+#include "Emergency.h"
+#include <string>
+#include <vector>
+
+
+class EmergencyIncident : public EmergencyIncident
+{
+    //members
+    private:
+        std::string location;
+        std:string description;
+        std::vector<Capability> baseCapabilities;
+
+    //methods
+    public:
+        EmergencyIncident(std::string, std::string, std::vector<Capability>);
+        ~EmergencyIncident();
+        std::vector<Capability> getRequiredCapabilities();
+        std::string getLocation();
+        std::string getDescription();
+
+
+};
+
+#endif
