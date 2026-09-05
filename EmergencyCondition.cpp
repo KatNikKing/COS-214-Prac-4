790eadd44dd1ad0e81f50014f15812b0197e4548
Markus Zeelie <mmzzeelie@gmail.com>
2026-09-05 07:56:47 +0200

    added EmergencyCondition.cpp

---
+++ EmergencyCondition.cpp
@@ -0,0 +1,21 @@
+#include "EmergencyCondition.h"
+
+EmergencyCondition::~EmergencyCondition(*Emergency emergency)
+{
+    this->emergency = emergency;
+}
+
+EmergencyCondition::~EmergencyCondition()
+{
+    delete emergency;
+}
+
+std::string EmergencyCondition::getLocation()
+{
+    return emergency->getLocation();
+}
+
+std::string EmergencyCondition::getDescription()
+{
+    return emergency->getDescription();
+}
